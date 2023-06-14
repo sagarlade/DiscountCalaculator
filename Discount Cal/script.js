6ca320a8a8c602d3f1a500ceee39c013d23d14ea
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2023-06-13 19:27:22 -0700

    Calculator Update
    Calculator Update

--- Discount Cal/script.js
+++ Discount Cal/script.js
@@ -1,7 +1,23 @@
-const getPrice = function () {
-    const bill = Number(document.querySelector("#bill").value);
-    const discount = Number(document.querySelector("#discount").value) / 100;
-    var totalValue = bill - (bill * discount);
-    document.querySelector("#total").innerHTML = `Total Discount Amount ==  ${totalValue}`;
-  };
-  
+
+
+const getPrice = function() {
+  const billInput = document.querySelector("#bill");
+  const discountInput = document.querySelector("#discount");
+
+  // Check if bill and discount inputs are valid numbers
+  const bill = Number(billInput.value);
+  const discount = Number(discountInput.value);
+  if (isNaN(bill) || isNaN(discount)) {
+    alert("Please enter valid numeric values for bill and discount.");
+    return;
+  }
+
+  // Check if discount is between 0 and 100 (inclusive)
+  if (discount < 0 || discount > 100) {
+    alert(`Enter Discount is not correct:  ${discount}`);
+    return;
+  }
+
+  const totalValue = bill - bill * (discount / 100);
+  document.querySelector("#total").innerHTML = `Total Discount Amount: ${totalValue}`;
+};
